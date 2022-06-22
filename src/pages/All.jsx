28daea844e8e9a007adf0a84099d7732d7fd5d37
Habibulloh <habibulloh4080@gmail.com>
2022-06-23 00:32:20 +0500

    ozgard

---
+++ src/pages/All.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Nav from './components/nav/nav'
+
+function All() {
+  return (
+    <div></div>
+  )
+}
+
+export default All
---
+++ src/pages/components/nav/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>nmadr</div>
+  )
+}
+
+export default Nav
